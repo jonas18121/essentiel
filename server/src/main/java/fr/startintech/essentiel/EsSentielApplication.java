e60ab370f5e12b81b8597f716ab5b4c248bccc3a
lucnicolas <l.nicolas@intech-sud.fr>
2020-10-27 14:09:52 +0100

    Creating Spring projet

---
+++ server/src/main/java/fr/startintech/essentiel/EsSentielApplication.java
@@ -0,0 +1,13 @@
+package fr.startintech.essentiel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EsSentielApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EsSentielApplication.class, args);
+    }
+
+}
---
+++ server/src/test/java/fr/startintech/essentiel/EsSentielApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.startintech.essentiel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EsSentielApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
