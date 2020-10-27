d6cac6f402c26d1d55684cb17bb0ec7b15e51d53
lucnicolas <l.nicolas@intech-sud.fr>
2020-10-27 16:25:12 +0100

    Renaming project app files

--- server/src/main/java/fr/startintech/essentiel/EssentielApplication.java
+++ server/src/main/java/fr/startintech/essentiel/EssentielApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class EsSentielApplication {
+public class EssentielApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(EsSentielApplication.class, args);
+        SpringApplication.run(EssentielApplication.class, args);
     }
 
 }
--- server/src/test/java/fr/startintech/essentiel/EssentielApplicationTests.java
+++ server/src/test/java/fr/startintech/essentiel/EssentielApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class EsSentielApplicationTests {
+class EssentielApplicationTests {
 
     @Test
     void contextLoads() {
