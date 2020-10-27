265a76581df535d2d35a763b20cf25a1a423645e
lucnicolas <l.nicolas@intech-sud.fr>
2020-10-27 16:24:25 +0100

    Creating exceptions

---
+++ server/src/main/java/fr/startintech/essentiel/exeption/InvalidEntryException.java
@@ -0,0 +1,21 @@
+package fr.startintech.essentiel.exeption;
+
+public class InvalidEntryException extends RuntimeException {
+
+    public InvalidEntryException() {
+        super();
+    }
+
+    public InvalidEntryException(final String message, final Throwable cause) {
+        super(message, cause);
+    }
+
+    public InvalidEntryException(final String message) {
+        super(message);
+    }
+
+    public InvalidEntryException(final Throwable cause) {
+        super(cause);
+    }
+
+}
