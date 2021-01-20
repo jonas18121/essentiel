a876ec1658ff117ab7f148844b142a612d917c8d
h-marti <43405279+h-marti@users.noreply.github.com>
2021-01-20 22:22:37 +0100

    Display informations about structure in map's popup (#18)

--- client/src/app/services/pop-up/pop-up.service.ts
+++ client/src/app/services/pop-up/pop-up.service.ts
@@ -10,10 +10,22 @@ export class PopUpService {
   // "data" is defined in marker-service as "s"
   makeStructuresPopup(data: any): string {
     return `` +
-      `<div>ID: ${ data.id }</div>` +
-      `<div>Name: ${ data.name }</div>` +
-      `<div>Address: ${ data.address }</div>` +
-      `<div>Coordinates: [ ${ data.longitude }, ${ data.latitude } ]</div>`;
-
+      //`<div>ID : ${ data.id }</div>` +
+      //`<br>` +
+      `<div>Nom : ${ data.name }</div>` +
+      `<div>Type : ${ data.type }</div>` +
+      `<div>Adresse : ${ data.street } ${ data.city } ${ data.zip }</div>` +
+      `<br>` +
+      `<div>Description : ${ data.description }</div>` +
+      `<br>` +
+      `<div>Contact</div>` +
+      `<div>Nom : ${ data.contactName }</div>` +
+      `<div>Fonction : ${ data.contactFunction }</div>` +
+      `<div>Tel : ${ data.phone }</div>` +
+      `<div>Email : ${ data.email }</div>` +
+      `<br>` +
+      `<div>Label / Agréement : ${ data.label }</div>` +
+      `<br>` +
+      `<div>Coordonées : [ ${ data.longitude }, ${ data.latitude } ]</div>`;
   }
 }
