64bc91566d28ae5088fde40b7c52daf80d7b4bde
lucnicolas <l.nicolas@intech-sud.fr>
2021-02-09 19:04:17 +0100

    hotfix: Fixing sending auth token

--- client/src/app/components/login/login.component.ts
+++ client/src/app/components/login/login.component.ts
@@ -29,8 +29,6 @@ export class LoginComponent implements OnInit {
   }
 
   onSubmit() {
-    console.log(this.form);
-
     this.loginInfo = new AuthLoginInfo(
       this.form.username,
       this.form.password);
--- client/src/app/services/event/event.service.ts
+++ client/src/app/services/event/event.service.ts
@@ -1,31 +1,42 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from "@angular/common/http";
+import {HttpClient, HttpHeaders} from "@angular/common/http";
 import { Observable } from "rxjs";
 import { Event } from "../../models/event/event";
+import {TokenStorageService} from "../auth/token-storage.service";
 
 @Injectable({
   providedIn: 'root'
 })
 export class EventService {
   private readonly eventUrl: string;
+  private tokenType  = 'Bearer ';
 
-  constructor(private http: HttpClient) {
+  constructor(private http: HttpClient, private tokenService: TokenStorageService) {
     this.eventUrl = 'http://localhost:8080/api/event/';
   }
 
   public findAll(): Observable<Event[]> {
-    return this.http.get<Event[]>(this.eventUrl);
+    const header = new HttpHeaders().set('Authorization', this.tokenType + this.tokenService.getToken());
+    const headers = { headers: header };
+    return this.http.get<Event[]>(this.eventUrl, headers);
   }
 
   public findById(EventId: number): Observable<Event> {
-    return this.http.get<Event>(this.eventUrl + EventId);
+    const header = new HttpHeaders().set('Authorization', this.tokenType + this.tokenService.getToken());
+    const headers = { headers: header };
+    return this.http.get<Event>(this.eventUrl + EventId, headers);
   }
 
   public save(Event: Event): Observable<Event> {
-    return this.http.post<Event>(this.eventUrl, Event);
+    const header = new HttpHeaders().set('Authorization', this.tokenType + this.tokenService.getToken());
+    const headers = { headers: header };
+    console.log(headers);
+    return this.http.post<Event>(this.eventUrl, Event, headers);
   }
 
   public delete(Event: Event): Observable<Event> {
-    return this.http.delete<Event>(this.eventUrl + Event.id);
+    const header = new HttpHeaders().set('Authorization', this.tokenType + this.tokenService.getToken());
+    const headers = { headers: header };
+    return this.http.delete<Event>(this.eventUrl + Event.id, headers);
   }
 }
